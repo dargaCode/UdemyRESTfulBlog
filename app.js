2b2822acbdd143535f304d40759b44c402082c34
dargaCode <darga.code@gmail.com>
2016-05-20 01:06:34 -0700

    Configure mongo/mongoose

--- app.js
+++ app.js
@@ -15,6 +15,15 @@ const mongoose = require('mongoose');
 const DEFAULT_PORT = 3000;
 const PORT = process.env.PORT || DEFAULT_PORT;
 const SERVER_MSG = `Serving ${pjson.name} on port ${PORT}`;
+const DEFAULT_MONGO_URL = 'mongodb://localhost/restful_blog';
+const MONGO_URL = process.env.MONGO_URL || DEFAULT_MONGO_URL;
+const blogSchema = new mongoose.Schema({
+  title: String,
+  image: String,
+  body: String,
+  created: {type: Date, default: Date.now}
+});
+const Blog = mongoose.model('blog', blogSchema);
 
 // SETTINGS
 
@@ -22,6 +31,7 @@ app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({extended: true}));
 ejs.delimiter = '?';
+mongoose.connect(MONGO_URL);
 
 // VARIABLES
 
